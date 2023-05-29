17d582fa1b0c13a81d15aaf631a725b0203d8459
mhieung <mhieu.nguyen2k@gmail.com>
2023-05-29 16:29:39 +0700

    rebuild

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://acf5b27d574e54bc38502a52c3de783f-1945256850.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
