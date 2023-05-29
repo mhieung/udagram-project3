2ca7c1f7ea0e8ba3c276cdff0f37e8af32ae1c62
mhieung <mhieu.nguyen2k@gmail.com>
2023-05-29 13:54:35 +0700

    build and use frontend v6

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://abe87a4ab97ea489e94c27eb8bd66d24-1550650203.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
